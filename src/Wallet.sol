ce9dabefbe851829f5121a22a7bab6561a6720b0
Ethan Davis <ethan@pop-os.localdomain>
2023-02-19 20:42:16 +0000

    added setter functionality

--- src/Wallet.sol
+++ src/Wallet.sol
@@ -17,14 +17,30 @@ contract Wallet {
     Transaction[] public transactions; // each executed only if enough required approvals
     mapping(uint => mapping(address => bool)) public approved; // txId -> owners -> approved
 
+    event AddOwner(address indexed owner, uint indexed txId);
     event Approve(address indexed owner, uint indexed txId);
+    event ChangeRequiredApprovals(uint requiredApprovals);
     event Deposit(address indexed sender, uint amount);
     event Execute(uint indexed txId);
+    event RemoveOwner(address indexed owner, uint indexed txId);
     event Revoke(address indexed owner, uint indexed txId);
     event Submit(uint indexed txId);
+    event SubmitAddOwner(address indexed owner, uint indexed txId);
+    event SubmitChangeRequiredApprovals(uint requiredApprovals);
+    event SubmitRemoveOwner(address indexed owner, uint indexed txId);
 
+    modifier notExecuted(uint _txId) {
+        require(!transactions[_txId].executed, "tx already executed");
+        _;
+    }
+    
     modifier onlyOwner() {
         require(isOwner[msg.sender], "not owner");
+        _;
+    }
+
+    modifier onlyWallet() {
+        require(msg.sender == address(this), "must be called internaly");
         _;
     }
 
@@ -33,22 +49,24 @@ contract Wallet {
         _;
     }
 
-    modifier notExecuted(uint _txId) {
-        require(!transactions[_txId].executed, "tx already executed");
+    modifier validRequiredApprovals(uint _numOwners, uint _requiredApprovals) {
+        require(
+            _requiredApprovals > 0 
+            && _requiredApprovals < _numOwners, 
+            "required approvals out of range"
+        ); // required approvals must be less than amount of owners
         _;
     }
 
-    constructor(address[] memory _owners, uint _requiredApprovals) {
+    constructor(address[] memory _owners, uint _requiredApprovals) 
+        validRequiredApprovals(_owners.length, _requiredApprovals) 
+    {
         require(
             _owners.length >= 2 
             && _owners.length <= 10, 
             "must have 2 <= owners <= 10"
         ); // max of 10 owners 
-        require(
-            _requiredApprovals > 0 
-            && _requiredApprovals < _owners.length, 
-            "required approvals out of range"
-        ); // required approvals must be less than amount of owners
+        
 
         for (uint i; i < _owners.length; i++) {
             require(_owners[i] != address(0), "invalid owner");
@@ -65,6 +83,8 @@ contract Wallet {
         emit Deposit(msg.sender, msg.value);
     }
 
+    /* OWNER ACTIONS */
+
     function approve(uint _txId) 
         public 
         onlyOwner 
@@ -74,7 +94,7 @@ contract Wallet {
         require(!approved[_txId][msg.sender], "tx already approved");
         approved[_txId][msg.sender] = true;
         emit Approve(msg.sender, _txId);
-        // IDEA: auto execute option
+        // IDEA: auto execute option after last approve
     }
 
     function execute(uint _txId) 
@@ -82,7 +102,7 @@ contract Wallet {
         txExists(_txId) 
         notExecuted(_txId)
     {
-        require(_getApprovalCount(_txId) >= requiredApprovals, "more approvals needed");
+        require(getApprovalCount(_txId) >= requiredApprovals, "more approvals needed");
         Transaction storage transaction = transactions[_txId];
 
         transaction.executed = true;
@@ -117,10 +137,91 @@ contract Wallet {
 
         uint _txId = transactions.length - 1;
         emit Submit(_txId);
-        approve(_txId); //
+        approve(_txId); 
     }
 
-    function _getApprovalCount(uint _txId) private view returns (uint count) {
+    /* SPECIAL SUBMITS */
+
+    function submitAddOwner(address _newOwner) external onlyOwner {
+        transactions.push(Transaction({
+            to: address(this),
+            value: 0,
+            data: abi.encodeWithSignature("addOwner(address)", _newOwner),
+            executed: false
+        })); 
+
+        uint _txId = transactions.length - 1;
+        emit SubmitAddOwner(_newOwner, _txId);
+        approve(_txId);
+    }
+
+    function submitChangeRequiredApprovals(uint _requiredApprovals) external onlyOwner {
+        transactions.push(Transaction({
+            to: address(this),
+            value: 0,
+            data: abi.encodeWithSignature("changeRequiredApprovals(uint)", _requiredApprovals),
+            executed: false
+        })); 
+
+        uint _txId = transactions.length - 1;
+        emit SubmitChangeRequiredApprovals(_requiredApprovals);
+        approve(_txId);
+    }
+
+    function submitRemoveOwner(address _owner) external onlyOwner {
+        transactions.push(Transaction({
+            to: address(this),
+            value: 0,
+            data: abi.encodeWithSignature("removeOwner(address)", _owner),
+            executed: false
+        })); 
+
+        uint _txId = transactions.length - 1;
+        emit SubmitRemoveOwner(_owner, _txId);
+        approve(_txId);
+    }
+
+    /* ONLY WALLET */
+
+    function addOwner(address _newOwner) external onlyWallet { 
+        require(_newOwner != address(0), "invalid owner");
+        require(!isOwner[_newOwner], "duplicate owner");
+        require(owners.length + 1 <= 10, "must have owners <= 10"); 
+
+        isOwner[_newOwner] = true;
+        owners.push(_newOwner);
+    }
+
+    function changeRequiredApprovals(uint _requiredApprovals) 
+        external 
+        onlyWallet 
+        validRequiredApprovals(owners.length, _requiredApprovals) 
+    {
+        requiredApprovals = _requiredApprovals;
+        emit ChangeRequiredApprovals(requiredApprovals);
+    }
+
+    function removeOwner(address _owner) 
+        external 
+        onlyWallet 
+        validRequiredApprovals(owners.length - 1, requiredApprovals) 
+    { 
+        require(isOwner[_owner], "not owner");
+        require(owners.length - 1 >= 2, "must have owners >= 2"); 
+
+        isOwner[_owner] = false;
+        for (uint i; i < owners.length - 1; i++) {
+            if (owners[i] == _owner) {
+                owners[i] = owners[owners.length - 1];
+                break;
+            }
+        }
+        owners.pop();
+    }
+
+    /* PRIVATE */
+
+    function getApprovalCount(uint _txId) private view returns (uint count) {
         for (uint i; i < owners.length; i++) {
             if (approved[_txId][owners[i]]) {
                 count += 1;
--- test/Wallet.t.sol
+++ test/Wallet.t.sol
@@ -9,10 +9,16 @@ contract WalletTest is Test {
     address public addr1;
     address public addr2;
     address[] public owners;
+    bool public executeFlag;
 
     function setUp() public {
         owners = [vm.addr(1), vm.addr(2), vm.addr(3), vm.addr(4)];
         wallet = new Wallet(owners, 3);
+        executeFlag = false;
+    }
+
+    function helperSetExecuteFlag() external {
+        executeFlag = true;
     }
 
     /* CONSTRUCTOR */
@@ -25,7 +31,7 @@ contract WalletTest is Test {
 
     function testTooManyOwners() public {
         address[] memory manyOwners = new address[](11);
-        for (uint i; i < 11; i++) { manyOwners[i] = address(0); }
+        for (uint i; i < 11; i++) manyOwners[i] = address(0);
         vm.expectRevert("must have 2 <= owners <= 10");
         new Wallet(manyOwners, 0);
     }
@@ -100,16 +106,167 @@ contract WalletTest is Test {
         uint txId = 0;
         vm.expectRevert("tx already approved");
         wallet.approve(txId);
+        vm.stopPrank();
     }
 
     function testApprovalSet() public {
+        vm.prank(owners[0]);
+        wallet.submit(owners[1], 0, "");
+        uint txId = 0;
+        vm.prank(owners[1]);
+        wallet.approve(txId);
+        assert(wallet.approved(txId, owners[1]));
+    }
+
+    /* EXECUTE */
+    
+    function testNotEnoughApprovals() public {
         vm.startPrank(owners[0]);
+        wallet.submit(owners[1], 0, "");
+        uint txId = 0;
+        vm.expectRevert("more approvals needed");
+        wallet.execute(txId);
+        vm.stopPrank();
+    }
+
+    function testTransactionExecuted() public {
+        vm.prank(owners[0]);
+        wallet.submit(owners[1], 0, "");
+        uint txId = 0;
+        for (uint i = 1; i < owners.length; i++) {
+            vm.prank(owners[i]);
+            wallet.approve(txId);
+        }
+        vm.prank(owners[0]);
+        wallet.execute(txId);
+        (,,, bool executed) = wallet.transactions(txId);
+        assert(executed);
+    }
+
+    function testExecuteCallFails() public {
+        vm.prank(owners[0]);
+        wallet.submit(address(this), 0, "");
+        uint txId = 0;
+        for (uint i = 1; i < owners.length; i++) {
+            vm.prank(owners[i]);
+            wallet.approve(txId);
+        }
+        vm.prank(owners[0]);
+        vm.expectRevert("tx failed");
+        wallet.execute(txId);
+    }
+
+    function testExecuteEtherSent() public {
+        payable(wallet).transfer(1 ether);
+        vm.prank(owners[0]);
+        wallet.submit(owners[1], 1 ether, "");
+        uint txId = 0;
+        for (uint i = 1; i < owners.length; i++) {
+            vm.prank(owners[i]);
+            wallet.approve(txId);
+        }
+        vm.prank(owners[0]);
+        wallet.execute(txId);
+        assertEq(owners[1].balance, 1 ether);
+    }
+
+    function testExecuteCallWithData() public {
+        vm.prank(owners[0]);
+        wallet.submit(address(this), 0, abi.encodeWithSignature("helperSetExecuteFlag()"));
+        uint txId = 0;
+        for (uint i = 1; i < owners.length; i++) {
+            vm.prank(owners[i]);
+            wallet.approve(txId);
+        }
+        vm.prank(owners[0]);
+        wallet.execute(txId);
+        assert(executeFlag);
+    }
+
+    /* REVOKE */
+
+    function testRevokeNotApproved() public {
+        vm.prank(owners[0]);
+        wallet.submit(owners[1], 0, "");
+        uint txId = 0;
+        vm.prank(owners[1]);
+        vm.expectRevert("tx not approved");
+        wallet.revoke(txId);
+    }
+
+    function testRevokeApproveSetFalse() public {
+        vm.prank(owners[0]);
+        wallet.submit(owners[1], 0, "");
+        uint txId = 0;
+        vm.prank(owners[0]);
+        wallet.revoke(txId);
+        assert(!wallet.approved(txId, owners[0]));
+    }
+
+    function testRevokeExecuteFails() public {
+        vm.prank(owners[0]);
+        wallet.submit(owners[1], 0, "");
+        uint txId = 0;
+        for (uint i = 2; i < owners.length; i++) {
+            vm.prank(owners[i]);
+            wallet.approve(txId);
+        }
+        vm.startPrank(owners[0]);
+        wallet.revoke(txId);
+        vm.expectRevert("more approvals needed");
+        wallet.execute(txId);
+        vm.stopPrank();
+    }
+
+    /* APPROVE */
+
+    function testSubmitTransactionRecorded() public {
+        vm.prank(owners[0]);
+        uint _value = 0;
+        bytes memory _data = "";
+        wallet.submit(owners[1], _value, _data);
+        uint txId = 0;
+        (address to, uint value, bytes memory data, bool executed) = wallet.transactions(txId);
+        assertEq(to, owners[1]);
+        assertEq(value, _value);
+        assertEq(data, _data);
+        assertEq(executed, false);
+    }
+
+    function testSubmitTransactionApprovedBySender() public {
+        vm.prank(owners[0]);
         wallet.submit(owners[1], 0, "");
         uint txId = 0;
         assert(wallet.approved(txId, owners[0]));
     }
 
-    /* EXECUTE */
-    
+    function testSubmitTransactionNotApprovedByNonSender() public {
+        vm.prank(owners[0]);
+        wallet.submit(owners[1], 0, "");
+        uint txId = 0;
+        for (uint i = 1; i < owners.length; i++) assert(!wallet.approved(txId, owners[i])); 
+    }
+
+    /* ADD OWNER */
+
+    function testAddOwner() public {
+        vm.prank(owners[0]);
+        wallet.submitAddOwner(address(this));
+        uint txId = 0;
+        for (uint i = 1; i < owners.length; i++) {
+            vm.prank(owners[i]);
+            wallet.approve(txId);
+        }
+        
+
+        vm.prank(owners[0]);
+        wallet.execute(txId);
+        (address to, uint value, bytes memory data, bool executed) = wallet.transactions(txId);
+        console.log(to);
+        console.log(executed);
+        for (uint i; i < 5; i++ ) console.log(wallet.owners(i));
+        console.log(address(this));
+        assertEq(data, "");
+    }
 
 }
